8d65b98de6c1337c8861ab3291612cb8d5929135
tylerccaceres <tyler.caceres@gmail.com>
2019-07-08 18:35:48 -0400

    created post request

--- express_server.js
+++ express_server.js
@@ -4,18 +4,26 @@ const PORT = 8080; //default port 8080
 
 app.set('view engine', 'ejs');
 
+/*
+When our browser submits a POST request, the data in the request body is sent as a Buffer.
+While this data type is great for transmitting data, it's not readable for us humans.
+To make this data readable, we will need to install another piece of middleware, body-parser.
+*/
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
+
 const urlDatabase = {
   b2xVn2: 'http://www.lighthouselabs.ca',
   '9sm5xK': 'http://www.google.com'
 };
 
-app.get('/', (req, res) => {
-  res.send('Hello');
-});
-
 app.get('/urls', (req, res) => {
   let templateVars = {urls: urlDatabase};
   res.render('urls_index', templateVars);
+});
+
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new');
 });
 
 app.get('/urls/:shortURL', (req, res) => {
@@ -23,14 +31,25 @@ app.get('/urls/:shortURL', (req, res) => {
   res.render('urls_show', templateVars);
 });
 
-app.get('/urls.json', (req, res) => {
-  res.json(urlDatabase);
+app.post('/urls', (req, res) => {
+  urlDatabase[generateRandomString()] = req.body.longURL;
+  res.send('ok');
 });
 
-app.get('/hello', (req, res) => {
-  res.send('<html><body>Hello <b>World</b></body></html>\n');
+app.get('/urls.json', (req, res) => {
+  res.json(urlDatabase);
 });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+const generateRandomString = () => {
+  let string = '';
+  let options = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+  for (let i = 0; i < 6; i++) {
+    string += options[Math.floor(Math.random() * options.length)];
+  }
+  return string;
+};
